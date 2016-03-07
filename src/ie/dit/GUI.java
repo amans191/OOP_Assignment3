7e1322d178b7c5104ae3c7382e3b060641af98cf
Eoin Smyth <c14427202@mydit.ie>
2016-03-07 11:10:10 +0000

    Initial GUI commit

---
+++ src/ie/dit/GUI.java
@@ -0,0 +1,19 @@
+package ie.dit;
+
+import processing.core.PApplet;
+
+/**
+ * Created by Eoin on 07/03/2016.
+ */
+public class GUI extends PApplet {
+
+    public void settings() {
+        size(500, 500);
+    }
+
+    public void draw(){
+        background(0);
+        stroke(255);
+
+    }
+}
---
+++ src/ie/dit/Main.java
@@ -0,0 +1,11 @@
+package ie.dit;
+
+import processing.core.PApplet;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String[] a = {"MAIN"};
+        PApplet.runSketch(a, new GUI());
+    }
+}
