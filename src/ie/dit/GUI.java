c6320c49ed0d8bc740a1df0f2e3e6271c3b03502
vardaan <vardaansharma96@gmail.com>
2016-03-25 15:47:11 +0000

    book both sides

--- src/ie/dit/GUI.java
+++ src/ie/dit/GUI.java
@@ -1,6 +1,6 @@
 package ie.dit;
 
-import processing.core.PApplet;
+import processing.core.*;
 
 /**
  * Created by Eoin on 07/03/2016.
@@ -8,7 +8,7 @@ import processing.core.PApplet;
 public class GUI extends PApplet {
 
     public void settings() {
-        size(800, 500);
+        size(800, 500 , P3D);
     }
 
     public void draw(){
@@ -16,6 +16,34 @@ public class GUI extends PApplet {
         stroke(255);
         fill(0);
 
+        float s = height/20;
+        float w = width/2;
+        //float h = height - s;
+        //line(w,s,w,height - s);
+        //curve(w,height, w,h, width-(width/20), h-5, width,height);
+
+        //for animation
+        /*
+        float h = height/20;
+        pushMatrix();
+        translate(width/2,0);
+        rotateY((float) (mouseX * (-0.01)));
+        beginShape();
+        line(0, height / 20 ,0, height - (height / 20) );
+        stroke(255);
+        //arc(3*(width/4),height, 2*sqrt(((81*height*height) + (25 *width *width))/400), 2*sqrt(((81*height*height) + (25 *width *width))/400), -PI,0);
+        //curve(width/4,height/4, width/2,height/20, width-(width/20),height/20, width+(width/4),height/4);
+        drawarc(0 ,h, width/2 - (width / 20) , h, 200);
+        drawarc(0 ,height -h, width/2 - (width / 20) , height-h, 200);
+        line(width/2 - (width / 20) ,h, width/2 - (width / 20), height - (height / 20));
+        //drawarc(width/2 , height-h , width - (width / 20), height - (h) , 200);
+
+        //curve(width/4,height +(height/4), width/2,height-(height/20), width-(width/20),height-(height/20), width+(width/4),height+(height/4) );
+        endShape();
+        popMatrix();
+        */
+
+         //draw right side
         for(int i = 0; i<20; i+=2)
         {
             float h = height/20;
@@ -25,16 +53,34 @@ public class GUI extends PApplet {
             //arc(3*(width/4),height, 2*sqrt(((81*height*height) + (25 *width *width))/400), 2*sqrt(((81*height*height) + (25 *width *width))/400), -PI,0);
             //curve(width/4,height/4, width/2,height/20, width-(width/20),height/20, width+(width/4),height/4);
             drawarc(width/2 ,h, width - (width / 20) , h, 200);
-            line(width - (width / 20) +i,h+i, width - (width / 20)+i, height - (height / 20) +i);
             drawarc(width/2 , height - (height / 20)+i , width - (width / 20)+i, height - (h)+i , 200-(i*12));
+            line(width - (width / 20) +i,h+i, width - (width / 20)+i, height - (height / 20) +i);
             //curve(width/4,height +(height/4), width/2,height-(height/20), width-(width/20),height-(height/20), width+(width/4),height+(height/4) );
+
+            endShape();
+        }
+
+        //draw left side
+        for(int i = 0; i<20; i+=2)
+        {
+            float h = height/20;
+            beginShape();
+            line(width / 2, height / 20 , width / 2, height - (height / 20) - i);
+            stroke(255);
+            //arc(3*(width/4),height, 2*sqrt(((81*height*height) + (25 *width *width))/400), 2*sqrt(((81*height*height) + (25 *width *width))/400), -PI,0);
+            //curve(width/4,height/4, width/2,height/20, width-(width/20),height/20, width+(width/4),height/4);
+            drawarc(width/2 ,h,  (width / 20) , h, 200);
+            drawarc(width/2 , height -(height / 20)+i ,(width / 20)-i, (height - (h))+i , 200-(i*12));
+            line((width / 20) -i,h+i, (width / 20)-i, height - (height / 20) +i);
+            //curve(width/4,height +(height/4), width/2,height-(height/20), width-(width/20),height-(height/20), width+(width/4),height+(height/4) );
+
             endShape();
         }
     }
 
     private void drawarc(float x1, float y1, float x2, float y2, float h)
     {
-        float l = (x1-x2)/2;
-        curve(x1-(l-h),y1+h,x1,y1,x2,y2,x2+(l-h),y2+h);
+        float l = (x2-x1)/2;
+        curve(x1-(l),y1+h,0,x1,y1,0,x2,y2,0,x2+(l-h),y2+h,0);
     }
 }
